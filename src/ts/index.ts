32f951d4331ef74aa5ba417f294cae73333bc243
Hasan Mothaffar <hasan.mozafar@gmail.com>
2021-05-19 16:40:37 +0300

    Reduce the dimensions of the canvas on smaller screens.

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,9 +1,22 @@
 import SnakeGame from "./classes/Game.js";
 import Snake from "./classes/Snake.js";
 import Canvas from "./classes/Canvas.js";
-import KeyboardHandler from "./classes/KeyboardHandler.js";
+import KeyboardHandler from "./classes/EventHandlers/KeyboardHandler.js";
 
-const canvas = new Canvas('snakeBoard', 500, 500);
+// Reduce the dimensions of the canvas on smaller screens. This only happens when the game is loaded.
+const mediaQuery = matchMedia('(max-width: 576px)'); 
+let canvasWidth: number;
+let canvasHeight: number;
+
+if (mediaQuery.matches) {
+	canvasWidth = canvasHeight = 300;
+}
+
+else {
+	canvasWidth = canvasHeight = 500;
+}	
+
+const canvas = new Canvas('snakeBoard', canvasWidth, canvasHeight);
 const snake = new Snake(20, 'darkgreen', 'yellow');
 const snakeGame = new SnakeGame(snake, canvas);
 
