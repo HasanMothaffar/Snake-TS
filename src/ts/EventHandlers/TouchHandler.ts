2d8a0cf4c7ca7a72d972cfbede05471a9bfd21cc
Hasan Mothaffar <hasan.mozafar@gmail.com>
2021-06-05 23:19:22 +0300

    Add basic support for mobile screens

--- src/ts/EventHandlers/TouchHandler.ts
+++ src/ts/EventHandlers/TouchHandler.ts
@@ -4,18 +4,29 @@ export default class TouchHandler {
 	/**
 	 * The div that contains control arrows for mobile screens.
 	 */
-	private mobileArrowsDiv: HTMLDivElement;
+	private mobileArrowsDiv = document.getElementById('mobile-arrows') as HTMLDivElement;
+
+	/**
+	 * The pause and start buttons on mobile screens.
+	 */
+	private gameControlButtons = document.querySelectorAll('.game-controls-container button') as NodeListOf<HTMLButtonElement>;
 
 	/**
 	 * Initializes touch event listeners for the snake game.
 	 * @param snakeGame Snake game instance to attach the event listeners to.
 	 */
 	constructor(private snakeGame: Game) {
-		this.mobileArrowsDiv = document.getElementById('mobile-arrows') as HTMLDivElement;
-		this.mobileArrowsDiv.addEventListener('click', this);
+		this.mobileArrowsDiv.addEventListener('click', (event: Event) => this.handleArrowsEvent(event));
+
+		this.gameControlButtons.forEach((button) => {
+			button.addEventListener('click', (event: Event) => {
+				const eventCode = (event.target as HTMLButtonElement).dataset.toggle!;
+				this.handleGameControlButtons(eventCode);
+			})
+		})
 	 }
 
-	public handleEvent(event: MouseEvent) {
+	private handleArrowsEvent(event: Event): void {
 		// Event target might be an svg element, but I want the parent button element instead.
 		const eventTarget = (event.target as HTMLDivElement).closest('button')!;
 		
@@ -23,4 +34,15 @@ export default class TouchHandler {
 			this.snakeGame.snake.changeDirection(eventTarget.dataset.code);
 		}
 	}
+
+	private handleGameControlButtons(buttonCode: string): void {
+		let code;
+		if (buttonCode === 'start') code = 'Space';
+		else if (buttonCode === 'restart') code = 'KeyR';
+
+		document.dispatchEvent(new KeyboardEvent('keydown', {
+			code: code
+		}))
+	}
+
 }
